18c4ff77618e426985927a0b78bf1b84967627e9
CFeeney333 <c.feeney333@gmail.com>
2022-09-16 15:24:12 +0100

    Fix bug in computer move
    The code was checking to see if the cell was equal to the 'none' symbol. However, it was the symbol property of the cell we were supposed to be comparing with. It was always evaluating to false, which happened to work out some of the time

--- main.js
+++ main.js
@@ -366,7 +366,7 @@ function computerMove() {
     // if there is a line with the active symbol and two free spaces, put the active symbol into one of those places
     for (let line of lines) {
         if (line.filter(cell => cell.symbol === activeSymbol).length === 1 && line.filter(cell => cell.symbol === GameSymbol.none).length === 2) {
-            line[0] === GameSymbol.none ? boardModel.setSymbol(activeSymbol, line[0].row, line[0].column) : boardModel.setSymbol(activeSymbol, line[2].row, line[2].column);
+            line[0].symbol === GameSymbol.none ? boardModel.setSymbol(activeSymbol, line[0].row, line[0].column) : boardModel.setSymbol(activeSymbol, line[2].row, line[2].column);
             return;
         }
     }
